cd5138f0a732703c3c36faa451bbdb0171fdda4e
ravivarmakv <rvvenkat@in.ibm.com>
2017-05-06 02:12:00 -0400

    Update Medical_History_code.go

--- Medical_History_code.go
+++ Medical_History_code.go
@@ -282,8 +282,9 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 //=================================================================================================================================
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	
-	caller, caller_affiliation
 	fmt.Println("query is running " + function)
+	
+	caller, caller_affiliation
 	
     logger.Debug("function: ", function)
     logger.Debug("caller: ", caller)
